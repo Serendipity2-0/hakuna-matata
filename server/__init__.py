977636e41a66eb3e3dd23f32e22c7dafccf353ff
SatayrthReconect <satyarth.raghuvanshi@reconect.ai>
2024-09-16 11:14:33 +0530

    👷 added server required files

---
+++ server/__init__.py
@@ -0,0 +1,4 @@
+from .api_router import APIRouter
+from .settings import settings
+
+__all__ = ["settings", "APIRouter"]
---
+++ server/settings.py
@@ -0,0 +1,35 @@
+import logging
+import pathlib
+from datetime import UTC, timezone
+from typing import Optional
+
+from dotenv import load_dotenv
+from pydantic import ValidationError
+from pydantic_settings import BaseSettings
+
+load_dotenv(verbose=True, override=True)
+
+ROOT = pathlib.Path(__file__).resolve().parent.parent
+log_format = "%(levelname) -10s %(funcName) " "-25s %(lineno) -1d: %(message)s"
+logging.basicConfig(level=logging.INFO, format=log_format)
+
+
+class Settings(BaseSettings):
+    ASSEMBLYAI_API_KEY: str
+    ASSEMBLYAI_BASE_URL: str
+    OPENAI_ORG_KEY: str
+    OPENAI_API_KEY: str
+
+    # OPTIONAL SETTINGS
+    LOGGER: Optional[logging.Logger] = logging.getLogger(__name__)
+    UPLOAD_DIR: str = f"{ROOT}/uploads"
+
+    class Config:
+        case_sensitive = False
+        env_file = f"{ROOT}/.env"
+
+
+try:
+    settings = Settings()
+except ValidationError as exc:
+    logging.getLogger(__name__).error(repr(exc.errors()))
