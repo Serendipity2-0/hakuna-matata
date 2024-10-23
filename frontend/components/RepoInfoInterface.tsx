86ac7b66e7d6f056636dc220143b5496317ce821
VarshitaTP <varshitats@gmail.com>
2024-10-23 17:32:42 +0530

    added some of file for gh-pages

--- frontend/components/RepoInfoInterface.tsx
+++
@@ -1,8 +0,0 @@
-export default function RepoInfoInterface() {
-  return (
-    <div className="p-4 bg-yellow-100 border border-yellow-400 rounded-md">
-      <h3 className="text-lg font-semibold mb-2">RepoInfo Interface</h3>
-      <p>This component is currently under development. It will provide repository information functionality soon.</p>
-    </div>
-  );
-}
